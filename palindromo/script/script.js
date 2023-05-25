c9a29dabc5d7ad279f5f5b05f10a0025206b4bd5
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-05-25 16:48:33 +0200

    palindromo exercise done

--- palindromo/script/script.js
+++ palindromo/script/script.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const inputParola = document.getElementById('parola');
+const sendButton = document.getElementById('send-parola');
+const risultato = document.getElementById('risultato')
+
+function palindromo(word){
+    let result;
+    let myArray = ((word).toLowerCase()).split('');
+    let reverseWord = (myArray.reverse()).join('');
+    if(word === reverseWord){
+        result = true;
+    } else{
+        result = false;
+    }
+    return result
+}
+
+sendButton.addEventListener('click', function(){
+    let parolaScelta = inputParola.value
+    if(palindromo((parolaScelta).toLowerCase()) === true){
+        risultato.innerHTML = `<h2>${parolaScelta} &Egrave; UNA PAROLA PALINDROMA</h2>`
+    } else{
+        risultato.innerHTML = `<h2>${parolaScelta} NON &Egrave; UNA PAROLA PALINDROMA</h2>`
+    }
+}
+)
