f426b3d25b888429a20c376228d6e50c81be274c
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-05-25 18:35:31 +0200

    exercise done

--- palindromo/script/script.js
+++ palindromo/script/script.js
@@ -2,7 +2,7 @@
 
 const inputParola = document.getElementById('parola');
 const sendButton = document.getElementById('send-parola');
-const risultato = document.getElementById('risultato')
+const risultato = document.getElementById('risultato');
 
 function palindromo(word){
     let result;
@@ -13,15 +13,16 @@ function palindromo(word){
     } else{
         result = false;
     }
-    return result
+    return result;
 }
 
 sendButton.addEventListener('click', function(){
-    let parolaScelta = inputParola.value
+    let parolaScelta = inputParola.value;
     if(palindromo((parolaScelta).toLowerCase()) === true){
-        risultato.innerHTML = `<h2>${parolaScelta} &Egrave; UNA PAROLA PALINDROMA</h2>`
+        risultato.innerHTML = `<h2>${parolaScelta} &Egrave; UNA PAROLA PALINDROMA</h2>`;
     } else{
-        risultato.innerHTML = `<h2>${parolaScelta} NON &Egrave; UNA PAROLA PALINDROMA</h2>`
+        risultato.innerHTML = `<h2>${parolaScelta} NON &Egrave; UNA PAROLA PALINDROMA</h2>`;
     }
+    inputParola.value = '';
 }
 )
--- pari-o-dispari/script/script.js
+++ pari-o-dispari/script/script.js
@@ -1,1 +1,44 @@
 "use strict";
+
+const sendButton = document.getElementById('send');
+const numberInput = document.getElementById('numero-scelto');
+const selectInput = document.getElementById('pari-dispari');
+const esito = document.getElementById('esito')
+const numeroShow = document.getElementById('numero-pc')
+
+function numeroCasuale(){
+    let randomNumber = Math.floor(Math.random() * 6);
+    return randomNumber;
+}
+
+
+function risultato(number,numberPc){
+    let result;
+    let sum = number + numberPc
+    if (sum % 2 === 0){
+        result = "pari";
+    } else{
+        result = "dispari";
+    }
+    return result;
+}
+
+sendButton.addEventListener('click', function(){
+    
+    let numeroScelto = parseInt(numberInput.value)
+    let selectScelto = selectInput.value
+    if(numeroScelto > 5){
+        alert('Il numero scelto deve essere compreso tra 0 e 5 (estremi compresi)')
+    } else if(isNaN(numeroScelto) || selectScelto === ""){
+        alert('Scegli dei valori validi')
+    } else{
+        let numeroPc = numeroCasuale()
+        if(risultato(numeroScelto,numeroPc) === selectScelto){
+            esito.innerHTML = `<span>HAI VINTO</span>`
+        } else {
+            esito.innerHTML = `<span>HA VINTO IL COMPUTER</span>`
+        }
+        numeroShow.innerHTML = numeroPc
+    }
+}
+)
